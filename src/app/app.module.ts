e840fd79064f3ac0c1a9deb871222fcca0e8ceab
NorbertStefanskiPXL <Norbert.Stefanski@student.pxl.be>
2022-11-24 10:44:17 +0100

    Added some info to player
    Added some visible information to the player page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { LayoutModule } from '@angular/cdk/layout';
+import { PlayerComponent } from './components/player/player.component';
 
 import { HttpClientModule } from '@angular/common/http';
 
@@ -31,6 +32,7 @@ import { HttpClientModule } from '@angular/common/http';
     HomeComponent,
     PlayersComponent,
     RegisterComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
