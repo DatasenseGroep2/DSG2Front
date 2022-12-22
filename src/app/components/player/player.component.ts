e6dc4f9c2a04f8c8cc5cded6c3b0caa8ae82198c
RubenSmetsPXL <Ruben.Smets@student.pxl.be>
2022-12-22 13:39:43 +0100

    Added winpercentage
    Added winpercentage to player component

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -2,21 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { FootballersService } from 'src/app/services/footballers.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { ChartConfiguration, ChartOptions, ChartType } from "chart.js";
-
+import { ChartConfiguration, ChartOptions, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.css']
+  styleUrls: ['./player.component.css'],
 })
 export class PlayerComponent implements OnInit {
-  footballer:any;
-  weight:any;
-  footballerMatches:any;
+  footballer: any;
+  weight: any;
+  footballerMatches: any;
   selectedValue: any;
   selectedObject: any;
   match: any;
+  winPercentage: any;
 
   public lineChartData: ChartConfiguration<'line'>['data'] = {
     labels: [],
@@ -27,53 +27,69 @@ export class PlayerComponent implements OnInit {
         fill: false,
         tension: 0.5,
         borderColor: 'red',
-        backgroundColor: 'rgba(255,30,100,0.9)'
-      }
-    ]
+        backgroundColor: 'rgba(255,30,100,0.9)',
+      },
+    ],
   };
   public lineChartOptions: ChartOptions<'line'> = {
-    responsive: false
+    responsive: false,
   };
   public lineChartLegend = true;
   constructor(
     private route: ActivatedRoute,
-    private service:FootballersService,
-    private location: Location) { }
+    private service: FootballersService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
     this.getFootballer();
     this.getFootballerWeight();
     this.getFootballerMatch();
+    this.getWinPercentage();
   }
   getFootballer() {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    (this.service.getFootballer(id))
-        .subscribe(response =>this.footballer = response);
+    this.service
+      .getFootballer(id)
+      .subscribe((response) => (this.footballer = response));
   }
 
   getFootballerWeight() {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.service.getFootballerWeight(id)
-        .subscribe(response => {
-          response.forEach(weight => {
-            if (weight.weight) {
-
-              this.lineChartData.labels?.push(weight.dateOfWeight);
-              this.lineChartData.datasets[0].data.push(weight.weight);
-            }
-          
-          })
-        });
-      
+    this.service.getFootballerWeight(id).subscribe((response) => {
+      response.forEach((weight) => {
+        if (weight.weight) {
+          this.lineChartData.labels?.push(weight.dateOfWeight);
+          this.lineChartData.datasets[0].data.push(weight.weight);
+        }
+      });
+    });
   }
 
-  getFootballerMatch(){
+  getFootballerMatch() {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    (this.service.getFootballerMatches(id))
-        .subscribe(response => this.footballerMatches = response);
+    this.service
+      .getFootballerMatches(id)
+      .subscribe((response) => (this.footballerMatches = response));
   }
 
-  setSelectedObject(){
-    this.selectedObject = this.footballerMatches.find((match: { dateOfMatch: Date; }) => match.dateOfMatch == this.selectedValue);
+  setSelectedObject() {
+    this.selectedObject = this.footballerMatches.find(
+      (match: { dateOfMatch: Date }) => match.dateOfMatch == this.selectedValue
+    );
+  }
+
+  getWinPercentage() {
+    let count = 0;
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.service.getFootballerMatches(id).subscribe((response) => {
+      response.forEach((footballerMatch) => {
+        if (footballerMatch.score - footballerMatch.opponentScore > 0) {
+          count++;
+        }
+      });
+      this.winPercentage = (count / response.length) * 100;
+    });
   }
 }
